4d4b43556b64708f322c684adab11d0ed4736ea5
JLP603 <60498189+JLP603@users.noreply.github.com>
2021-02-03 11:03:41 +0800

    fixes

--- public/js/q2.js
+++ public/js/q2.js
@@ -2,10 +2,10 @@
 $(document).ready(function () {
     $("#btn-enter").click(() => {
         var state = $("#state").val();
-        var category = $("#category_input").val();
+        var category = $("#category").val();
         var day_of_week = $("#day_of_week").val();
         var path = "/query2output-" +state +"-"+ category +"-"+ day_of_week;
-        if ( state_input == ""||category == ""||day_of_week == "")
+        if ( state == ""||category == ""||day_of_week == "")
         {
             alert("Please fill up the field");
         }
